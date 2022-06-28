ec70af068e430112341a8663859177c421e15876
Shravan Shandilya <shravan.shandilya@setu.co>
2022-06-28 19:14:53 +0530

    ref: author details are nested inside comments

--- database/repositories/comment.js
+++ database/repositories/comment.js
@@ -2,11 +2,21 @@ import db from "../index.js";
 const comments = () => db("comments");
 
 async function createComment(user_id, content) {
-  return await comments().insert({ user_id, content }, ["id"]);
+  return await db("comments").insert({ user_id, content }, ["id"]);
 }
 
 async function fetchComments() {
-  return await comments().join("users", "comments.user_id", "=", "users.id");
+  return await db("comments")
+    .select(
+      "comments.id",
+      "content",
+      "comments.created_at",
+      "comments.updated_at",
+      db.raw(
+        `json_build_object('id', "user_id", 'fname', "fname", 'sname', "sname", 'display_picture', "display_picture") as "author"`
+      )
+    )
+    .join("users", "comments.user_id", "=", "users.id");
 }
 
 export default { createComment, fetchComments };
